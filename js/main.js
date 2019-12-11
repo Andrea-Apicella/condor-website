3d8b7ab175d7490b9f054df44b200c6b9dda98ac
Andrea Apicella <andreaapicella@MacBook-Pro-di-Andrea.local>
2019-12-11 16:27:14 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -1,28 +1,25 @@
 $(document).ready(function() {
+  //collapse navbar when a link is clicked
+  var scroll = new SmoothScroll('a[href*="#"]');
 
-//modal loads the right image
-$(".gallery-image").click(function() {
-  $('.modal-body').empty();
-  var image = $(this).attr("src");
-  var html ='<img src="'+image+'" style="width:100%"/>';
-  $('.modal-body').html(html);
-  $('#galleryModal').modal({show:true});
+  //modal loads the right image
+  $(".gallery-image").click(function() {
+    $(".modal-body").empty();
+    var image = $(this).attr("src");
+    var html = '<img src="' + image + '" style="width:100%"/>';
+    $(".modal-body").html(html);
+    $("#galleryModal").modal({ show: true });
+  });
+  //collapse navbar after a link is clicked
+  $(".navbar-collapse a").click(function() {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  //add active class on click
+  $(document).on("click", "nav a", function() {
+    $(this)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+  });
 });
-//collapse navbar after a link is clicked
-$(".navbar-collapse a").click(function() {
-  $(".navbar-collapse").collapse("hide");
-});
-
-//add active class on click
-$(document).on("click", "nav a", function() {
-  $(this)
-    .addClass("active")
-    .siblings()
-    .removeClass("active");
-});
-
-//collapse navbar when a link is clicked
-var scroll = new SmoothScroll('a[href*="#"]');
-
-});
-
